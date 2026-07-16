1b37a85e57328cc45252755e283ba1b8764bc1bc
Light <danil.odinzov181@gmail.com>
2026-07-16 22:36:04 +0500

    Добавление функционала:
    - Добавлен RootRepository

---
+++ app/domain/repositories/root.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from uuid import UUID
+
+from app.domain.entities.root.models import Root
+from app.domain.repositories.base import BaseRepository
+
+
+class RootRepository(BaseRepository[Root]):
+    async def get_all_by_node_id(self, node_id: UUID) -> list[Root]: ...
+    async def get_by_path_by_node_id(self, path: Path, node_id: UUID) -> Root | None: ...
+    async def get_by_alias(self, alias: str) -> Root | None: ...
