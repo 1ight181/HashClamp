a777e7a4d9093434d412bfc993a0b6295425cc64
Light <danil.odinzov181@gmail.com>
2026-07-18 00:19:34 +0500

    Добавление функционала:
    - Добавлена модель конфига для БД

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,21 @@ class UserSettings(BaseModel):
     default_notify_on_changes: bool = False
 
 
+class DatabaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=True,
+    )
+
+    user: str
+    password: str
+    host: str
+    port: int
+    database_name : str
+
+    def get_postgres_dsn(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database_name}"
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -18,9 +33,9 @@ class Settings(BaseSettings):
     )
 
     debug: bool = True
-    database_dsn: str
 
     user_settings: UserSettings = UserSettings()
+    db_settings: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
