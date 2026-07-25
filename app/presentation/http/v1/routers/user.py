7ba90c8a07f66a42493955eacda7ff002da580a8
Light <danil.odinzov181@gmail.com>
2026-07-25 23:31:20 +0300

    feat: add new routs for user
    - create_user
    - update_user

--- app/presentation/http/v1/routers/user.py
+++ app/presentation/http/v1/routers/user.py
@@ -3,9 +3,12 @@ from uuid import UUID
 from fastapi import APIRouter
 from fastapi import Depends
 
+from app.application.cmd.create_user import CreateUserCommand
+from app.application.cmd.update_user import UpdateUserCommand
 from app.application.services.user import UserService
+
 from app.presentation.http.v1.deps.user import get_user_service
-from app.presentation.http.v1.schemas.user import UserResponse
+from app.presentation.http.v1.schemas.user import UserResponse, UserCreateRequest, UserUpdateRequest
 
 router = APIRouter(
     prefix="/users",
@@ -18,3 +21,29 @@ async def get_user(
     service: UserService = Depends(get_user_service)
 ):
     return await service.get_user(user_id)
+
+
+@router.post("", response_model=UserResponse)
+async def create_user(
+    user_create_req: UserCreateRequest,
+    service: UserService = Depends(get_user_service),
+) -> UserResponse:
+    cmd = CreateUserCommand(
+        **user_create_req.model_dump(exclude_unset=True),
+    )
+
+    created_user = await service.create_user(cmd)
+
+    return UserResponse.model_validate(created_user)
+
+@router.put("/{user_id}", response_model=UserResponse)
+async def update_user(
+    user_id: UUID,
+    user_update_req: UserUpdateRequest,
+    service: UserService = Depends(get_user_service),
+):
+    cmd = UpdateUserCommand(
+        changes=user_update_req.model_dump(exclude_unset=True),
+    )
+
+    return await service.update_user(user_id, cmd)
