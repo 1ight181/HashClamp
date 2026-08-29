66c801611213614f44c834cd3536422d8798222f
Light <danil.odinzov181@gmail.com>
2026-08-29 22:19:57 +0300

    refactor: move EntityNotFoundError to shared module

--- app/presentation/http/v1/error_handlers/registration.py
+++ app/presentation/http/v1/error_handlers/registration.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.application.exceptions.not_found import EntityNotFoundError
+from app.shared.exceptions.not_found import EntityNotFoundError
 from app.domain.entities.exceptions import DomainInvalidDataError
 from app.presentation.http.v1.error_handlers.not_found import entity_not_found_handler
 from app.presentation.http.v1.error_handlers.user import domain_invalid_data_error_handler
